3bf5befc5302c231ae688151c9c9e60c4a622c18
MarkPoloChina <1228479724@qq.com>
2024-07-23 03:32:06 +0800

    [update] cancel cors.

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v4');
-  app.enableCors({
-    origin: 'https://www.markpolo.cn',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'https://www.markpolo.cn',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
   await app.listen(3000);
 }
 bootstrap();
